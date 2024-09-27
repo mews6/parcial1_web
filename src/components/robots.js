9f7f34ecd888ae43ee6ceecf5661aa10938bf661
Jaime Torres <jatber.mrowr@gmail.com>
2024-09-27 11:34:35 -0500

    add table skeleton

--- src/components/robots.js
+++ src/components/robots.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Table } from 'react-bootstrap';
 import {FormattedMessage} from 'react-intl';
 
 function Robots(){
@@ -14,6 +15,30 @@ function Robots(){
     
     const robotData = {...robots}
     console.log(robotData);
+
+    return(
+        <Table>
+            <thead>
+                <th>id</th>
+                <th>Nombre</th>
+                <th>modelo</th>
+                <th>Empresa Fabricante</th>
+            </thead>
+            <tbody>
+                
+                {robots.map(robot => 
+                    <tr>
+                        <td>{robot.id}</td>
+                        <td>{robot.nombre}</td>
+                        <td>{robot.modelo}</td>
+                        <td>{robot.empresaFabricante}</td>
+                    </tr>
+                )}
+                
+            </tbody>
+        </Table>
+    )
+
 }
 
 export default Robots;
